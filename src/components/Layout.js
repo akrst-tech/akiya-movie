8a4d19685fda0b73fbe82433cb101d8c023d709e
Akira Suto <suto1208>
2020-03-15 20:07:09 +0900

    Ignore case

---
+++ src/components/Layout.js
@@ -0,0 +1,71 @@
+import React from "react"
+import styled from "styled-components"
+
+import MobileMenu from "./MobileMenu"
+import Footer from "./Footer"
+import TopSection from "./TopSection"
+import DesktopMenu from "./DesktopMenu"
+
+// import NavBar from "./NavBar"
+
+import "./layout.css"
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  width: 90vw;
+  max-width: 1000px;
+  margin: 0 auto;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`
+
+const BottomSection = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const VideoSection = styled.div`
+  width: 100%;
+  @media screen and (min-width: 700px) {
+    width: 80%;
+  }
+`
+
+const Layout = ({ location, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let topSection
+
+  if (location.pathname === rootPath) {
+    topSection = <TopSection />
+  } else {
+    topSection = null
+  }
+  return (
+    <div>
+      <div>{topSection}</div>
+      <MobileMenu />
+      <Wrapper>
+        <BottomSection>
+          <DesktopMenu />
+          <VideoSection>{children}</VideoSection>
+        </BottomSection>
+        <Footer />
+      </Wrapper>
+    </div>
+  )
+}
+
+export default Layout
+
+// <div>
+//   <TopSection />
+//   <MobileMenu />
+//   <Wrapper>
+//     <BottomSection>
+//       <DesktopMenu />
+//       <VideoSection>{children}</VideoSection>
+//     </BottomSection>
+//     <Footer />
+//   </Wrapper>
+// </div>
